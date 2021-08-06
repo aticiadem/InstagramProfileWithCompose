95aff2c2e7a53ddc56f33084866f24d2edc9f8a9
adematici <adematicis41@gmail.com>
2021-08-06 23:42:43 +0300

    TopBar and ProfileSection added

--- app/src/main/java/com/adematici/instagramprofilewithcompose/MainActivity.kt
+++ app/src/main/java/com/adematici/instagramprofilewithcompose/MainActivity.kt
@@ -14,25 +14,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            InstagramProfileWithComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            ProfileScreen()
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    InstagramProfileWithComposeTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/adematici/instagramprofilewithcompose/ProfileScreen.kt
@@ -0,0 +1,143 @@
+package com.adematici.instagramprofilewithcompose
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun ProfileScreen(){
+    Column(modifier = Modifier.fillMaxSize()) {
+        TopBar(name = "adematiciii",modifier = Modifier.padding(16.dp))
+        Spacer(modifier = Modifier.height(4.dp))
+        ProfileSection()
+    }
+}
+
+@Composable
+fun TopBar(
+    name: String,
+    modifier: Modifier = Modifier
+){
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = modifier
+            .fillMaxWidth()
+    ) {
+        Icon(
+            imageVector = Icons.Default.ArrowBack,
+            contentDescription = "Back",
+            tint = Color.Black,
+            modifier = Modifier.size(32.dp)
+        )
+        Text(
+            text = name,
+            overflow = TextOverflow.Ellipsis,
+            fontWeight = FontWeight.Bold,
+            fontSize = 22.sp
+        )
+        Icon(
+            painter = painterResource(id = R.drawable.ic_bell),
+            contentDescription = "Back",
+            tint = Color.Black,
+            modifier = Modifier.size(28.dp)
+        )
+        Icon(
+            painter = painterResource(id = R.drawable.ic_dotmenu),
+            contentDescription = "Back",
+            tint = Color.Black,
+            modifier = Modifier.size(32.dp)
+        )
+    }
+}
+
+@Composable
+fun ProfileSection(){
+    Column(Modifier.fillMaxWidth()) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp)
+        ) {
+            RoundImage(
+                image = painterResource(id = R.drawable.adem),
+                modifier = Modifier
+                    .size(100.dp)
+                    .weight(3f)
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            StatSection(modifier = Modifier.weight(7f))
+        }
+    }
+}
+
+@Composable
+fun RoundImage(
+    image: Painter,
+    modifier: Modifier = Modifier
+){
+    Image(
+        painter = image,
+        contentDescription = null,
+        modifier = modifier
+            .aspectRatio(1f, matchHeightConstraintsFirst = true)
+            .border(
+                width = 1.dp,
+                color = Color.LightGray,
+                shape = CircleShape
+            )
+            .padding(3.dp)
+            .clip(CircleShape)
+    )
+}
+
+@Composable
+fun StatSection(modifier: Modifier = Modifier){
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceAround,
+        modifier = modifier
+    ) {
+        ProfileStat(numberText = "0", text = "Posts")
+        ProfileStat(numberText = "346", text = "Followers")
+        ProfileStat(numberText = "368", text = "Following")
+    }
+}
+
+@Composable
+fun ProfileStat(
+    numberText: String,
+    text: String,
+    modifier: Modifier = Modifier
+){
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ) {
+        Text(
+            text = numberText,
+            fontWeight = FontWeight.Bold,
+            fontSize = 20.sp
+        )
+        Spacer(modifier = Modifier.height(4.dp))
+        Text(text = text)
+    }
+}
